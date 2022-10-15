fb61acaa59bff2a9ee7be38daba0adf83b5d7b61
Renato Costa <renatoaccosta@gmail.com>
2022-10-15 12:19:48 +0100

    remove many people route

--- myApp.js
+++ myApp.js
@@ -91,8 +91,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (error, data) => {
+    if (error) return console.log(error);
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
